b8483aeca1b1a45cb75e5afff66698969b82f567
redstorm <air_forge@bk.ru>
2021-10-18 16:05:36 +0500

    Добавьте файлы проекта.

---
+++ HomeWorkConsoleApp8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
