bc80329c1a01972f969d9ff7ab06206373bcb525
redstorm <air_forge@bk.ru>
2021-10-18 16:18:59 +0500

    Добавленно задание 1

--- HomeWorkConsoleApp8/Program.cs
+++ HomeWorkConsoleApp8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HomeWorkConsoleApp8
 {
@@ -6,7 +8,42 @@ namespace HomeWorkConsoleApp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Задание 1. Работа с листом.
+            //Задание 2.Телефонная книга.
+            //Задание 3.Проверка повторов.
+            //Задание 4.Записная книжка.
+
+            //Задание 1.Работа с листом
+            //Что нужно сделать
+            //Создайте лист целых чисел.
+            //Заполните лист 100 случайными числами в диапазоне от 0 до 100.
+            //Выведите коллекцию чисел на экран.
+            //Удалите из листа числа, которые больше 25, но меньше 50.
+            //Снова выведите числа на экран.
+            List<int> numberList = new();
+            Random random = new();
+            Console.WriteLine("100 случайными числами в диапазоне от 0 до 100");
+            for (int i = 0; i < 100; i++)
+            {
+                numberList.Add(random.Next(100));
+            }
+
+            foreach (var item in numberList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("больше 25, но меньше 50");
+            var t = numberList.RemoveAll(e=>e>25&&e<50);
+
+            foreach (var item in numberList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
+
+        
     }
 }
